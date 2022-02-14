0e478b99eebb124b84d4a2a21891308afb40080a
sangram <sks98.sahoo@gmail.com>
2022-02-14 20:20:49 +0530

    initial commit

---
+++ main.js
@@ -0,0 +1,80 @@
+
+
+
+
+
+
+
+
+//fetch the product total amount
+var product_total_amt=document.getElementById("product_total_amt");
+
+//for total cart amouont
+var shipping_charge=document.getElementById('shipping_charge');
+var total_cart_amt=document.getElementById('total_cart_amt');
+
+//discoutn code
+var discountCode=document.getElementById("discount_code1");
+
+
+
+const decreaseNumber=(incdec,itemprice)=>{
+    var itemval=document.getElementById(incdec);
+    var myprice=document.getElementById(itemprice);
+    console.log(itemval.value);
+    if(itemval.value <= 0){
+        itemval.value=0;
+        alert("negative value not allowed")
+    }else{
+        itemval.value=parseInt(itemval.value)-1;
+        itemval.style.background="#fff";
+        itemval.style.color="#000";
+        myprice.innerHTML=parseInt(myprice.innerHTML)-100;
+        product_total_amt.innerHTML=parseInt(product_total_amt.innerHTML)-100;
+
+        total_cart_amt.innerHTML=parseInt(product_total_amt.innerHTML) +  
+        parseInt(shipping_charge.innerHTML);
+        
+
+
+    }
+}
+
+
+
+const increaseNumber=(incdec,itemprice)=>{
+    var itemval=document.getElementById(incdec);
+    var myprice=document.getElementById(itemprice);
+    if(itemval.value >= 5){
+        itemval.value=5;
+        alert("maximum 5 piece is allowed");
+        itemval.style.background="red";
+        itemval.style.color="#fff";
+    }else{
+        itemval.value=parseInt(itemval.value)+1;
+        myprice.innerHTML=parseInt(myprice.innerHTML)+100;
+        product_total_amt.innerHTML=parseInt(product_total_amt.innerHTML)+100;
+
+        total_cart_amt.innerHTML=parseInt(product_total_amt.innerHTML)+  
+        parseInt(shipping_charge.innerHTML);
+
+    }
+}
+
+//discount
+const discount_code=()=>{
+    let totalamtcurr=parseInt(total_cart_amt.innerHTML);
+    let error_trw =document.getElementById('error_trw');
+    if(discountCode.value === 'Watch87'){
+        let newtotalamt= totalamtcurr - 15;
+        total_cart_amt.innerHTML = newtotalamt;
+        error_trw.innerHTML= 'code is valid ';
+    }else{
+        error_trw.innerHTML= 'code is invallid';
+
+    }
+}
+
+
+
+
